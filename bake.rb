8252c1a891bc98556e6db573db1c96f1ab1526da
Matheus Richard <matheusrichardt@gmail.com>
2020-07-16 23:33:34 -0300

    Update bake tasks

--- bake.rb
+++ bake.rb
@@ -1,12 +1,19 @@
 require 'date'
 
+# Runs development server
+def s
+  system 'npx gulp'
+end
+
+# Creates a new post
+# 
 # @param post_name [String] name of the post to be created.
-# @param categores [Array(String)] categories of the post.
-def post(post_name, categories: 'ruby')
+# @param tags [Array(String)] tags of the post.
+def post(post_name, tags: 'ruby')
   time = Time.now
   file_name = time.to_date.to_s + '-' + Slugify.(post_name) + '.md'
 
-  File.write(POSTS_PATH + file_name, FileContent.(post_name, time, categories))
+  File.write(POSTS_PATH + file_name, FileContent.(post_name, time, tags))
 end
 
 # Constants
@@ -15,13 +22,13 @@ POSTS_PATH = '_posts/'
 # Helpers
 Slugify = ->(str) { str.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '') }
 Titleize = ->(str) { str.split.map { |word| word.capitalize }.join(' ')  }
-FileContent = ->(post_name, time, categories) do
+FileContent = ->(post_name, time, tags) do
   <<~CONTENT
   ---
   layout: post
   title: "#{Titleize.(post_name)}"
   date: #{time.to_s}
-  categories: #{categories.split.map { |word| word.downcase }.join(' ')}
+  categories: #{tags.join(' ')}
   ---
   CONTENT
 end
