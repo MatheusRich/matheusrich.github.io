c8687d53fc17d2511f6d024d0109aa5798c53217
Matheus Richard <matheusrichardt@gmail.com>
2021-11-06 17:06:56 -0300

    Add light mode to code blocks
    Closes #18

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -3,6 +3,6 @@ export function dimNonHighlightedLines() {
   const highlightSpans = document.querySelectorAll('pre code span.hll');
 
   highlightSpans.forEach(highlightedSpan => {
-    highlightedSpan.parentNode.style.filter = 'opacity(0.5)';
+    highlightedSpan.parentNode.style.filter = 'opacity(var(--code-opacity))';
   });
 }
