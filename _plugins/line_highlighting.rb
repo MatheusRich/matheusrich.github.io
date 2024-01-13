6c9cfe0f1138e8468bd69f2b6b1950e474b9671d
Matheus Richard <matheusrichardt@gmail.com>
2024-01-12 21:45:28 -0300

    Remove unused code

--- _plugins/line_highlighting.rb
+++ _plugins/line_highlighting.rb
@@ -23,12 +23,3 @@ module Jekyll
     end
   end
 end
-
-Jekyll::Hooks.register :documents, :pre_render do |document, payload|
-  doc_ext = document.extname.tr(".", "")
-
-  # only process if we deal with a markdown file
-  if payload["site"]["markdown_ext"].include? doc_ext
-    document.content.gsub!(/^```([A-z]+){([\d\s]+)}$(.*?)^```$/im, '{% highlight \1 highlight_lines="\2" %}\3{% endhighlight %}')
-  end
-end
